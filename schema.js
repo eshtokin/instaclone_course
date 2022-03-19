e15931f5a59cce139ed9528f2afadab59422e0fe
Alex Eshtokin <alexeshtokin@machine.local>
2022-03-20 00:16:33 +0200

    move from apollo-server to apollo-server-express

--- schema.js
+++ schema.js
@@ -1,8 +1,15 @@
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
+import { getUserFromToken } from './Users/users.utils'
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`)
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`)
 
 export const typeDefs = mergeTypeDefs(loadedTypes)
 export const resolvers = mergeResolvers(loadedResolvers)
+
+export const context = async ({ req }) => {
+  return {
+    loggedUser: await getUserFromToken(req.headers.token),
+  }
+}
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 require('dotenv').config()
-import { ApolloServer } from 'apollo-server'
-import { typeDefs, resolvers } from './schema'
-import { getUserFromToken } from './Users/users.utils'
+import http from 'http'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import { typeDefs, resolvers, context } from './schema'
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUserFromToken(req.headers.token),
-    }
-  },
-})
 
-const PORT = process.env.PORT
+;(async function startApolloServer(typeDefs, resolvers, context) {
+  const PORT = process.env.PORT
+  const app = express()
+  const httpServer = http.createServer(app)
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
 
-server.listen(PORT).then(() => {
-  console.log(`sercer is run on http://localhost:${PORT}/`)
-})
+  await server.start()
+  server.applyMiddleware({ app })
+
+  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve))
+  console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+})(typeDefs, resolvers, context)
